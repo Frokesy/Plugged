a81dbd30762a04e6ed875215d4f8965c2189f371
Frokesy <akindeleayanfeoluwa@gmail.com>
2024-08-19 09:32:59 +0100

    ready to publish test v0.2.0

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
+import { resolve } from 'node:path'
 import react from '@vitejs/plugin-react'
+import { defineConfig, ConfigEnv, LibraryFormats } from 'vite'
+import dts from 'vite-plugin-dts'
+import EsLint from 'vite-plugin-linter'
+import tsConfigPaths from 'vite-tsconfig-paths'
+import * as packageJson from './package.json'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+const { EsLinter, linterPlugin } = EsLint
+
+export default defineConfig((configEnv: ConfigEnv) => ({
+  plugins: [
+    react(),
+    tsConfigPaths(),
+    linterPlugin({
+      include: ['./src}/**/*.{ts,tsx}'],
+      linters: [new EsLinter({ configEnv })],
+    }),
+    dts({
+      include: ['src/component/'],
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve('src', 'components/index.ts'),
+      name: 'paginato',
+      formats: ['es', 'umd'] as LibraryFormats[],
+      fileName: (format: string) => `paginato.${format}.js`,
+    },
+    rollupOptions: {
+      external: [...Object.keys(packageJson.devDependencies || {})],
+    },
+  },
+}))
