5ea0a93793f9f646f6b397ddab9a9018eb02c6fb
Frokesy <akindeleayanfeoluwa@gmail.com>
2024-08-19 21:39:10 +0100

    removed prettier and setup tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Paginator } from "./components"
 
 function App() {
 
   return (
     <>
       <div className="">
-        <h2>Hello world</h2>
+        <h2 className="text-3xl text-red-500">Hello world</h2>
+        <Paginator totalPages={10} currentPage={1} />
       </div>
     </>
   )
